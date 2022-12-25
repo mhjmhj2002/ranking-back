f863a4e5bc4a5522841b836c708239c431b150a0
mhj <mhjmhj2002@gmail.com>
2022-12-25 15:15:26 -0300

    add swagger

--- ranking-webapp/src/main/java/com/mhj/ranking/config/SwaggerConfig.java
+++ ranking-webapp/src/main/java/com/mhj/ranking/config/SwaggerConfig.java
@@ -1,28 +1,19 @@
 package com.mhj.ranking.config;
 
-import java.util.List;
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
-import org.springframework.web.bind.annotation.RequestMethod;
 
-import io.swagger.models.auth.In;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.builders.ResponseMessageBuilder;
-import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.Contact;
-import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -33,13 +24,13 @@ public class SwaggerConfig {
     public Docket api() { 
         return new Docket(DocumentationType.SWAGGER_2)  
           .select()                                  
-          .apis(RequestHandlerSelectors.basePackage("br.com.treinaweb.springbootapi.controller"))    
+          .apis(RequestHandlerSelectors.basePackage("com.mhj.ranking.controller"))    
           .paths(PathSelectors.any())
           .build()
           .useDefaultResponseMessages(false)                                   
-          .globalResponseMessage(RequestMethod.GET, responseMessageForGET())
-          .securitySchemes(Arrays.asList(new ApiKey("Token Access", HttpHeaders.AUTHORIZATION, In.HEADER.name())))
-          .securityContexts(Arrays.asList(securityContext()))
+//          .globalResponseMessage(RequestMethod.GET, responseMessageForGET())
+//          .securitySchemes(Arrays.asList(new ApiKey("Token Access", HttpHeaders.AUTHORIZATION, In.HEADER.name())))
+//          .securityContexts(Arrays.asList(securityContext()))
           .apiInfo(apiInfo());
     }
 
@@ -54,31 +45,7 @@ public class SwaggerConfig {
                 .build();
     }
 
-    private List<ResponseMessage> responseMessageForGET()
-    {
-        return new ArrayList<ResponseMessage>() {
-            private static final long serialVersionUID = 1L;
-
-            {
-            add(new ResponseMessageBuilder()   
-                .code(500)
-                .message("500 message")
-                .responseModel(new ModelRef("Error"))
-                .build());
-            add(new ResponseMessageBuilder() 
-                .code(403)
-                .message("Forbidden!")
-                .build());
-        }};
-    }
-
-    private SecurityContext securityContext() {
-        return SecurityContext.builder()
-            .securityReferences(defaultAuth())
-            .forPaths(PathSelectors.ant("/pessoa/**"))
-            .build();
-    }
-    
+        
     List<SecurityReference> defaultAuth() {
         AuthorizationScope authorizationScope
             = new AuthorizationScope("ADMIN", "accessEverything");
