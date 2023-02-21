aa3b94c912a28857648bfec9e0bc756e75e87c0f
mhj <mhjmhj2002@gmail.com>
2023-02-21 18:36:52 -0300

    add h2 connection

--- ranking-core/src/main/java/com/mhj/ranking/config/DataSourceConfiguration.java
+++ ranking-core/src/main/java/com/mhj/ranking/config/DataSourceConfiguration.java
@@ -8,16 +8,16 @@ import org.springframework.context.annotation.Configuration;
 
 import com.zaxxer.hikari.HikariDataSource;
 
-@Configuration
+//@Configuration
 public class DataSourceConfiguration {
 
-	@Bean(name = "customDataSource")
-	@ConfigurationProperties("spring.datasource")
+//	@Bean(name = "customDataSource")
+//	@ConfigurationProperties("spring.datasource")
 	public DataSource customDataSource() {
 //		return DataSourceBuilder.create().build();
 
 		HikariDataSource hikari = new HikariDataSource();
-		hikari.setJdbcUrl("jdbc:mysql://localhost:3306/mhj06?useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true&useSSL=false");
+		hikari.setJdbcUrl("jdbc:mysql://localhost:3306/xxx?useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true&useSSL=false");
 		hikari.addDataSourceProperty("driverClassName", "com.mysql.jdbc.Driver");
 		hikari.addDataSourceProperty("username", "root");
 		hikari.addDataSourceProperty("password", "2y244y26");
